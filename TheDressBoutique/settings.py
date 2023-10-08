7e19ff8ba5a35030b4cb4e1f6b41ed672f921a7a
Rehab Saleh <mohelmoniry@gmail.com>
2023-10-08 17:16:33 +0300

    apdate

--- TheDressBoutique/settings.py
+++ TheDressBoutique/settings.py
@@ -92,25 +92,37 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+#if 'RDS_DB_NAME' in os.environ:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql',
+#            'NAME': os.environ['RDS_DB_NAME'],
+#            'USER': os.environ['RDS_USERNAME'],
+#            'PASSWORD': os.environ['RDS_PASSWORD'],
+#             'HOST': os.environ['RDS_HOSTNAME'],
+#            'PORT': os.environ['RDS_PORT'],
+#        }
+#    }
+#else:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ebdb',
+        'USER': 'djangoadmin',
+        'PASSWORD': 'Not64_No32',
+        'HOST': 'awseb-e-ccpdzfajim-stack-awsebrdsdatabase-cjajro6spboa.cckzo6recldk.us-west-2.rds.amazonaws.com',
+        'PORT': 5432,
+    }
+}
 
 AUTH_PROFILE_MODULE = 'my_profile_app.UserProfile'
 # Password validation
