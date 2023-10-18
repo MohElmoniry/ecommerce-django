67a436bb463d8cecb13b96fa7c93de5ac96b80fa
Rehab Saleh <mohelmoniry@gmail.com>
2023-10-18 09:44:24 +0300

    apdate

--- TheDressBoutique/static/js/paymob.js
+++ TheDressBoutique/static/js/paymob.js
@@ -4,29 +4,13 @@
 	var payment_method = 'PayMob'
     var redirect_url = "{% url 'order_complete' %}"
     var url = "{% url 'payments' %}"
-    var csrftoken = getCookie('csrftoken');
 
 
 
     const API = 'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2T1RBME1qY3hMQ0p1WVcxbElqb2lhVzVwZEdsaGJDSjkuOW9FOG9GUTI2MFlBdm1yZHNsaDJVcE50dzhIdDFPUU9fUDE3N3UtOUJPb3ZOVnIzZTU4ckVKUWdfVV9nT3Axc1Rna3dleDNoN1JDNVY1ekpjMDJNQWc='        // your api here
     const integrationID = 2627644;
 
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    
+
     async function firstStep () {
         let data = {
             "api_key": API
@@ -105,42 +89,6 @@
         location.href = iframURL
     }
 
-    async function cerat_order(data, actions){
-        return actions.order.create({
-            purchase_units: [{
-                amount: {
-                    value: amount,
-                }
-            }]
-        });
-    }
 
-    async function datadb(data, actions){
-        return actions.order.capture().then(function(details) {
-            // Show a success message to the buyer
-            console.log(details);
-            sendData();
-            function sendData(){
-                fetch(url, {
-                    method : "POST",
-                    headers: {
-                        "Content-type": "application/json",
-                        "X-CSRFToken": csrftoken,
-                    },
-                    body: JSON.stringify({
-                        orderID: orderID,
-                        transID: details.id,
-                        payment_method: payment_method,
-                        status: details.status,
-                    }),
-                })
-              .then((response) => response.json())
-              .then((data) => {
-                    window.location.href = redirect_url + '?order_number='+data.order_number+'&payment_id='+data.transID;
-                });
-            }
-        });
 
-    }
-
-    
+ 
